8fa8df8c139044cda3ae910bf09fd0d476989704
cheonmro <cheonmro@gmail.com>
2018-07-02 07:45:25 +0900

    Add array elements challenge for finding the largest number that meets a condition

---
+++ challenges/largestNumbers.js
@@ -0,0 +1,36 @@
+// In a given integer array nums, there is always exactly one largest element.
+
+// Find whether the largest element in the array is at least twice as much as every other number in the array.
+
+// If it is, return the index of the largest element, otherwise return -1.
+
+// Example 1:
+
+// Input: nums = [3, 6, 1, 0]
+// Output: 1
+// Explanation: 6 is the largest integer, and for every other number in the array x,
+// 6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+ 
+
+// Example 2:
+
+// Input: nums = [1, 2, 3, 4]
+// Output: -1
+// Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+ 
+
+// Note:
+
+// nums will have a length in the range [1, 50].
+// Every nums[i] will be an integer in the range [0, 99].
+
+var dominantIndex = function(nums) {
+    // ES5: Math.max.apply(null, nums) -> ES6: Math.max(...nums)
+    const largeElem = Math.max(...nums); // 6
+    let count = 0;
+    nums.forEach(elem => largeElem >= elem * 2 ? count++ : 0);
+    return (count === nums.length - 1) ? nums.indexOf(largeElem) : -1;
+};
+
+console.log(dominantIndex([3, 6, 1, 0])); // 1
+console.log(dominantIndex([1, 2, 3, 4])); // -1
