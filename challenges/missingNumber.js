9fbd5ae303bf225de31c8128c07b824b7cfbebd4
cheonmro <cheonmro@gmail.com>
2018-07-10 19:10:00 +0900

    Add array challenge for finding the number that it is included in the array

---
+++ challenges/missingNumber.js
@@ -0,0 +1,22 @@
+// Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+// Example 1:
+// Input: [3,0,1]
+// Output: 2
+
+// Example 2:
+// Input: [9,6,4,2,3,5,7,0,1]
+// Output: 8
+
+// Note:
+// Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+
+var missingNumber = function(nums) {
+  for(let i = 0; i < nums.length; i++) {
+    if(!nums.includes(i)) {
+      return i;
+    }
+  }
+};
+console.log(missingNumber([3,0,1])); // 2
+console.log(missingNumber([9,6,4,2,3,5,7,0,1])); // 8
